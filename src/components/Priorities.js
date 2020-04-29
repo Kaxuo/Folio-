64ec36e05dc81c966ddab8da98f3351d7fd8c883
Your Name <you@example.com>
2020-04-29 19:05:02 +0200

    first commit

--- src/components/Priorities.js
+++ src/components/Priorities.js
@@ -5,6 +5,7 @@ import { FcMultipleSmartphones } from "react-icons/fc"
 import { AiFillCodeSandboxCircle } from "react-icons/ai"
 import { FiUsers } from "react-icons/fi"
 import Grid from '@material-ui/core/Grid';
+import Fade from 'react-reveal/Fade';
 
 function Priorities() {
     const useStyles = makeStyles((theme) => ({
@@ -12,9 +13,9 @@ function Priorities() {
             backgroundColor: "#1B242F",
             width: "100%",
             height: "100%",
-            padding:"2vh",
-            flexGrow:1,
-            overflow:"hidden"
+            padding: "2vh",
+            flexGrow: 1,
+            overflow: "hidden"
         },
         title: {
             textAlign: "center",
@@ -37,20 +38,20 @@ function Priorities() {
                 borderBottom: "5px solid white",
             }
         },
-        icon:{
-            fontSize:"300%",
+        icon: {
+            fontSize: "300%",
             // if you wanna move the hexagon
             // marginTop:"20%",
-            bottom:"15px",
-            position:"relative",
-            left:"24px"
+            bottom: "15px",
+            position: "relative",
+            left: "24px"
         },
-        grid:{
-            paddingTop:"5vh",
+        grid: {
+            paddingTop: "5vh",
         },
-        text:{
-            textAlign:"center",
-            padding:"3vh",
+        text: {
+            textAlign: "center",
+            padding: "3vh",
             fontFamily: 'Arimo',
         },
     }))
@@ -59,42 +60,51 @@ function Priorities() {
     return (
         <div className={classes.page}>
             <div className={classes.title} ><h1>Priorities</h1></div>
-            <Grid className={classes.grid} justify="center"  container spacing={2}>
-                <Grid  item  xs ={12} md={2} >
-                    <div className="hexagon">
-                        <p className={classes.icon}> <GiBookCover/></p>
-                    </div>
-                    <div className={classes.text}>
-                    <h3 style={{color:"white", fontWeight:"bold"}}>Open-minded</h3>
-                    <p style={{color:"white"}}>Always eager to learn new languages when needed to complete a task</p>
-                    </div>
+
+            <Grid className={classes.grid} justify="center" container spacing={2}>
+                <Grid item xs={12} md={2} >
+                    <Fade left>
+                        <div className="hexagon">
+                            <p className={classes.icon}> <GiBookCover /></p>
+                        </div>
+                        <div className={classes.text}>
+                            <h3 style={{ color: "white", fontWeight: "bold" }}>Open-minded</h3>
+                            <p style={{ color: "white" }}>Always eager to learn new languages when needed to complete a task</p>
+                        </div>
+                    </Fade>
                 </Grid>
-                <Grid  item  xs ={12} md={2} >
-                    <div className="hexagon">
-                        <p className={classes.icon}> <FcMultipleSmartphones /></p>
-                    </div>
-                    <div className={classes.text}>
-                    <h3 style={{color:"white", fontWeight:"bold"}}> Responsive</h3>
-                    <p style={{color:"white"}}>My layouts will work on any device, big or small.</p>
-                    </div>
+                <Grid item xs={12} md={2} >
+                    <Fade left>
+                        <div className="hexagon">
+                            <p className={classes.icon}> <FcMultipleSmartphones /></p>
+                        </div>
+                        <div className={classes.text}>
+                            <h3 style={{ color: "white", fontWeight: "bold" }}> Responsive</h3>
+                            <p style={{ color: "white" }}>My layouts will work on any device, big or small.</p>
+                        </div>
+                    </Fade>
                 </Grid>
-                <Grid  item  xs ={12} md={2} >
-                    <div className="hexagon">
-                        <p className={classes.icon}> <AiFillCodeSandboxCircle/></p>
-                    </div>
-                    <div className={classes.text}>
-                    <h3 style={{color:"white", fontWeight:"bold"}}>Structure</h3>
-                    <p style={{color:"white"}}>Code easy to read and websites user friendly </p>
-                    </div>
+                <Grid item xs={12} md={2} >
+                    <Fade right>
+                        <div className="hexagon">
+                            <p className={classes.icon}> <AiFillCodeSandboxCircle /></p>
+                        </div>
+                        <div className={classes.text}>
+                            <h3 style={{ color: "white", fontWeight: "bold" }}>Structure</h3>
+                            <p style={{ color: "white" }}>Code easy to read and websites user friendly </p>
+                        </div>
+                    </Fade>
                 </Grid>
-                <Grid  item  xs ={12} md={2} >
+                <Grid item xs={12} md={2} >
+                <Fade right>
                     <div className="hexagon">
-                        <p className={classes.icon}> <FiUsers/></p>
+                        <p className={classes.icon}> <FiUsers /></p>
                     </div>
                     <div className={classes.text}>
-                    <h3 style={{color:"white", fontWeight:"bold"}}> Dynamic</h3>
-                    <p style={{color:"white"}}>A dynamic website is much more enjoyable than a static one</p>
+                        <h3 style={{ color: "white", fontWeight: "bold" }}> Dynamic</h3>
+                        <p style={{ color: "white" }}>A dynamic website is much more enjoyable than a static one</p>
                     </div>
+                    </Fade>
                 </Grid>
             </Grid>
         </div>
